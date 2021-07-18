8f4c42038a89821002cca571fd1bf048ba06981b
a6888b <74986761+a6888b@users.noreply.github.com>
2021-07-18 21:16:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let number__ = [1,2,3,4,5,6,7,8,9,0];
+function choice(i){
+    return Math.floor(Math.random() * i);
+}
+
+function Jeux(){
+    const NumberMyster= choice(number__.length)
+    let FoundNumberMyster = false
+    alert(NumberMyster);
+    while(!FoundNumberMyster){
+        let choice = prompt("Bienvenue au jeux du nombre myster,Veuiller entrer un nombre pour deviner:");
+        if (choice != Number){
+            alert('Il ya un probleme,entrez un nombre');
+            continue;
+        }
+        if (choice == NumberMyster){
+            alert("Vous avez trouver le nombre mystere !!");
+            FoundNumberMyster = true;
+        }else if (NumberMyster < choice){
+            alert(`le nombre mystere est plus petit ${choice}`);
+        }else if (NumberMyster > choice){
+            alert(`le nombre mystere est plus grand ${choice}`);
+        }
+    }
+}
+console.log(Jeux())
